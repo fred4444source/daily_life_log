623fbe17341b7585ca52cf6371b44a813b8be6fc
fred4444source <fred4444source@sina.com>
2020-11-17 20:20:50 +0800

    add 民法典 部分内容

---
+++ category/china_civil_code/china_civil_code.js
@@ -0,0 +1,592 @@
+{
+	let json_data = {
+		text_content : [
+			{p : 0, t : '中华人民共和国民法典'},
+		],
+		level : '',
+		children : [
+			{
+				text_content : [
+					{p : 0, t : '第一编　总则'},
+				],
+				level : '总编',
+				children : [
+					{
+						text_content : [
+							{p : 0, t : '第一章　基本规定'},
+						],
+						level : '章',
+						children : [
+							{
+								text_content : [
+									{p : 0, t : '第一条　为了保护民事主体的合法权益，调整民事关系，维护社会和经济秩序，适应中国特色社会主义发展要求，弘扬社会主义核心价值观，根据宪法，制定本法。'},
+								],
+								level : '条',
+								rule_number : 13,
+							},
+							{
+								text_content : [
+									{p : 0, t : '第二条　民法调整平等主体的自然人、法人和非法人组织之间的人身关系和财产关系。'},
+								],
+								level : '条',
+								rule_number : 14,
+							},
+							{
+								text_content : [
+									{p : 0, t : '第三条　民事主体的人身权利、财产权利以及其他合法权益受法律保护，任何组织或者个人不得侵犯。'},
+								],
+								level : '条',
+								rule_number : 15,
+							},
+							{
+								text_content : [
+									{p : 0, t : '第四条　民事主体在民事活动中的法律地位一律平等。'},
+								],
+								level : '条',
+								rule_number : 16,
+							},
+							{
+								text_content : [
+									{p : 0, t : '第五条　民事主体从事民事活动，应当遵循自愿原则，按照自己的意思设立、变更、终止民事法律关系。'},
+								],
+								level : '条',
+								rule_number : 17,
+							},
+							{
+								text_content : [
+									{p : 0, t : '第六条　民事主体从事民事活动，应当遵循公平原则，合理确定各方的权利和义务。'},
+								],
+								level : '条',
+								rule_number : 18,
+							},
+							{
+								text_content : [
+									{p : 0, t : '第七条　民事主体从事民事活动，应当遵循诚信原则，秉持诚实，恪守承诺。'},
+								],
+								level : '条',
+								rule_number : 19,
+							},
+							{
+								text_content : [
+									{p : 0, t : '第八条　民事主体从事民事活动，不得违反法律，不得违背公序良俗。'},
+								],
+								level : '条',
+								rule_number : 20,
+							},
+							{
+								text_content : [
+									{p : 0, t : '第九条　民事主体从事民事活动，应当有利于节约资源、保护生态环境。'},
+								],
+								level : '条',
+								rule_number : 21,
+							},
+							{
+								text_content : [
+									{p : 0, t : '第十条　处理民事纠纷，应当依照法律；法律没有规定的，可以适用习惯，但是不得违背公序良俗。'},
+								],
+								level : '条',
+								rule_number : 22,
+							},
+							{
+								text_content : [
+									{p : 0, t : '第十一条　其他法律对民事关系有特别规定的，依照其规定。'},
+								],
+								level : '条',
+								rule_number : 23,
+							},
+							{
+								text_content : [
+									{p : 0, t : '第十二条　中华人民共和国领域内的民事活动，适用中华人民共和国法律。法律另有规定的，依照其规定。'},
+								],
+								level : '条',
+								rule_number : 24,
+							},
+						],
+					},
+					{
+						text_content : [
+							{p : 0, t : '第二章　自然人'},
+						],
+						level : '章',
+						children : [
+							{
+								text_content : '第一节　民事权利能力和民事行为能力',
+								level : '节',
+								children : [
+									{
+										text_content : [
+											{p : 0, t : '第十三条　自然人从出生时起到死亡时止，具有民事权利能力，依法享有民事权利，承担民事义务。'},
+										],
+										level : '条',
+										rule_number : 13,
+									},
+									{
+										text_content : [
+											{p : 0, t : '第十四条　自然人的民事权利能力一律平等。'},
+										],
+										level : '条',
+										rule_number : 14,
+									},
+									{
+										text_content : [
+											{p : 0, t : '第十五条　自然人的出生时间和死亡时间，以出生证明、死亡证明记载的时间为准；没有出生证明、死亡证明的，以户籍登记或者其他有效身份登记记载的时间为准。有其他证据足以推翻以上记载时间的，以该证据证明的时间为准。'},
+										],
+										level : '条',
+										rule_number : 15,
+									},
+									{
+										text_content : [
+											{p : 0, t : '第十六条　涉及遗产继承、接受赠与等胎儿利益保护的，胎儿视为具有民事权利能力。但是，胎儿娩出时为死体的，其民事权利能力自始不存在。'},
+										],
+										level : '条',
+										rule_number : 16,
+									},
+									{
+										text_content : [
+											{p : 0, t : '第十七条　十八周岁以上的自然人为成年人。不满十八周岁的自然人为未成年人。'},
+										],
+										level : '条',
+										rule_number : 17,
+									},
+									{
+										text_content : [
+											{p : 0, t : '第十八条　成年人为完全民事行为能力人，可以独立实施民事法律行为。'},
+											{p : 4, t : '十六周岁以上的未成年人，以自己的劳动收入为主要生活来源的，视为完全民事行为能力人。'},
+										],
+										level : '条',
+										rule_number : 18,
+									},
+									{
+										text_content : [
+											{p : 0, t : '第十九条　八周岁以上的未成年人为限制民事行为能力人，实施民事法律行为由其法定代理人代理或者经其法定代理人同意、追认；但是，可以独立实施纯获利益的民事法律行为或者与其年龄、智力相适应的民事法律行为。'},
+										],
+										level : '条',
+										rule_number : 19,
+									},
+									{
+										text_content : [
+											{p : 0, t : '第二十条　不满八周岁的未成年人为无民事行为能力人，由其法定代理人代理实施民事法律行为。'},
+										],
+										level : '条',
+										rule_number : 20,
+									},
+									{
+										text_content : [
+											{p : 0, t : '第二十一条　不能辨认自己行为的成年人为无民事行为能力人，由其法定代理人代理实施民事法律行为。'},
+											{p : 5, t : '八周岁以上的未成年人不能辨认自己行为的，适用前款规定。'},
+										],
+										level : '条',
+										rule_number : 21,
+									},
+									{
+										text_content : [
+											{p : 0, t : '第二十二条　不能完全辨认自己行为的成年人为限制民事行为能力人，实施民事法律行为由其法定代理人代理或者经其法定代理人同意、追认；但是，可以独立实施纯获利益的民事法律行为或者与其智力、精神健康状况相适应的民事法律行为。'},
+										],
+										level : '条',
+										rule_number : 22,
+									},
+									{
+										text_content : [
+											{p : 0, t : '第二十三条　无民事行为能力人、限制民事行为能力人的监护人是其法定代理人。'},
+										],
+										level : '条',
+										rule_number : 23,
+									},
+									{
+										text_content : [
+											{p : 0, t : '第二十四条　不能辨认或者不能完全辨认自己行为的成年人，其利害关系人或者有关组织，可以向人民法院申请认定该成年人为无民事行为能力人或者限制民事行为能力人。'},
+											{p : 5, t : '被人民法院认定为无民事行为能力人或者限制民事行为能力人的，经本人、利害关系人或者有关组织申请，人民法院可以根据其智力、精神健康恢复的状况，认定该成年人恢复为限制民事行为能力人或者完全民事行为能力人。'},
+											{p : 5, t : '本条规定的有关组织包括：居民委员会、村民委员会、学校、医疗机构、妇女联合会、残疾人联合会、依法设立的老年人组织、民政部门等。'},
+										],
+										level : '条',
+										rule_number : 24,
+									},
+									{
+										text_content : [
+											{p : 0, t : '第二十五条　自然人以户籍登记或者其他有效身份登记记载的居所为住所；经常居所与住所不一致的，经常居所视为住所。'},
+										],
+										level : '条',
+										rule_number : 25,
+									},
+								],
+							},
+							{
+								text_content : '第二节　监护',
+								level : '节',
+								children : [
+									{
+									},
+								],
+							},
+							{
+								text_content : '第三节　宣告失踪和宣告死亡',
+								level : '节',
+								children : [
+									{
+									},
+								],
+							},
+							{
+								text_content : '第四节　个体工商户和农村承包经营户',
+								level : '节',
+								children : [
+									{
+									},
+								],
+							},
+						],
+					},
+					{
+						text_content : '第三章　法人',
+						level : '章',
+						children : [
+							{
+								text_content : '第一节　一般规定',
+								level : '节',
+								children : [
+								],
+							},
+							{
+								text_content : '第二节　营利法人',
+								level : '节',
+								children : [
+								],
+							},
+							{
+								text_content : '第三节　非营利法人',
+								level : '节',
+								children : [
+								],
+							},
+							{
+								text_content : '第四节　特别法人',
+								level : '节',
+								children : [
+								],
+							},
+						],
+					},
+					{
+						text_content : '第四章　非法人组织',
+						level : '章',
+						children : [
+						],
+					},
+					{
+						text_content : '第五章　民事权利',
+						level : '章',
+						children : [
+						],
+					},
+					{
+						text_content : '第六章　民事法律行为',
+						level : '章',
+						children : [
+							{
+								text_content : '第一节　一般规定',
+								level : '节',
+								children : [
+								],
+							},
+							{
+								text_content : '第二节　意思表示',
+								level : '节',
+								children : [
+								],
+							},
+							{
+								text_content : '第三节　民事法律行为的效力',
+								level : '节',
+								children : [
+								],
+							},
+							{
+								text_content : '第四节　民事法律行为的附条件和附期限',
+								level : '节',
+								children : [
+								],
+							},
+						],
+					},
+					{
+						text_content : '',
+						level : '章',
+						children : [
+							{
+								text_content : '',
+								level : '节',
+								children : [
+								],
+							},
+						],
+					},
+					{
+						text_content : '',
+						level : '章',
+						children : [
+						],
+					},
+					{
+						text_content : '',
+						level : '章',
+						children : [
+						],
+					},
+					{
+						text_content : '',
+						level : '章',
+						children : [
+						],
+					},
+				]
+			},
+		],
+	};
+/*
+第七章　代理
+第一节　一般规定
+第二节　委托代理
+第三节　代理终止
+第八章　民事责任
+第九章　诉讼时效
+第十章　期间计算
+第二编　物权
+第一分编　通则
+第一章　一般规定
+第二章　物权的设立、变更、转让和消灭
+第一节　不动产登记
+第二节　动产交付
+第三节　其他规定
+第三章　物权的保护
+第二分编　所有权
+第四章　一般规定
+第五章　国家所有权和集体所有权、私人所有权
+第六章　业主的建筑物区分所有权
+第七章　相邻关系
+第八章　共有
+第九章　所有权取得的特别规定
+第三分编　用益物权
+第十章　一般规定
+第十一章　土地承包经营权
+第十二章　建设用地使用权
+第十三章　宅基地使用权
+第十四章　居住权
+第十五章　地役权
+第四分编　担保物权
+第十六章　一般规定
+第十七章　抵押权
+第一节　一般抵押权
+第二节　最高额抵押权
+第十八章　质权
+第一节　动产质权
+第二节　权利质权
+第十九章　留置权
+第五分编　占有
+第二十章　占有
+第三编　合同
+第一分编　通则
+第一章　一般规定
+第二章　合同的订立
+第三章　合同的效力
+第四章　合同的履行
+第五章　合同的保全
+第六章　合同的变更和转让
+第七章　合同的权利义务终止
+第八章　违约责任
+第二分编　典型合同
+第九章　买卖合同
+第十章　供用电、水、气、热力合同
+第十一章　赠与合同
+第十二章　借款合同
+第十三章　保证合同
+第一节　一般规定
+第二节　保证责任
+第十四章　租赁合同
+第十五章　融资租赁合同
+第十六章　保理合同
+第十七章　承揽合同
+第十八章　建设工程合同
+第十九章　运输合同
+第一节　一般规定
+第二节　客运合同
+第三节　货运合同
+第四节　多式联运合同
+第二十章　技术合同
+第一节　一般规定
+第二节　技术开发合同
+第三节　技术转让合同和技术许可合同
+第四节　技术咨询合同和技术服务合同
+第二十一章　保管合同
+第二十二章　仓储合同
+第二十三章　委托合同
+第二十四章　物业服务合同
+第二十五章　行纪合同
+第二十六章　中介合同
+第二十七章　合伙合同
+第三分编　准合同
+第二十八章　无因管理
+第二十九章　不当得利
+第四编　人格权
+第一章　一般规定
+第二章　生命权、身体权和健康权
+第三章　姓名权和名称权
+第四章　肖像权
+第五章　名誉权和荣誉权
+第六章　隐私权和个人信息保护
+第五编　婚姻家庭
+第一章　一般规定
+第二章　结婚
+第三章　家庭关系
+第一节　夫妻关系
+第二节　父母子女关系和其他近亲属关系
+第四章　离婚
+第五章　收养
+第一节　收养关系的成立
+第二节　收养的效力
+第三节　收养关系的解除
+第六编　继承
+第一章　一般规定
+第二章　法定继承
+第三章　遗嘱继承和遗赠
+第四章　遗产的处理
+第七编　侵权责任
+第一章　一般规定
+第二章　损害赔偿
+第三章　责任主体的特殊规定
+第四章　产品责任
+第五章　机动车交通事故责任
+第六章　医疗损害责任
+第七章　环境污染和生态破坏责任
+第八章　高度危险责任
+第九章　饲养动物损害责任
+第十章　建筑物和物件损害责任
+附则
+//*/
+	
+	(function() {
+		const script_tag = document.currentScript;
+		const root_div = script_tag.parentNode;
+
+		const fold_data_array = [];
+		const div_fold_all = document.createElement('div');
+		const div_expend_all = document.createElement('div');
+		const div_expend_before_rule = document.createElement('div');
+		
+		const fold_expend_all = function(data, expend, level) {
+			const has_children = data.children && data.children.length;
+			if (level && has_children) {
+				if (level === data.children[0].level) {
+					if (data.children_hidden) {
+					} else {
+						data.dom.content_dom.click();
+					}
+					return;
+				}
+			}
+			if (data.children_hidden) {
+				if (expend) {
+					data.dom.content_dom.click();
+				}
+			} else {
+				if (expend) {
+				} else {
+					data.dom.content_dom.click();
+				}
+			}
+			if (has_children) {
+				data.children.forEach((d) => {
+					fold_expend_all(d, expend, level);
+				});
+			}
+		};
+		div_fold_all.addEventListener('click', function(e) {
+			fold_expend_all(json_data, false);
+		});
+		div_expend_all.addEventListener('click', function(e) { 
+			fold_expend_all(json_data, true);
+		});
+		div_expend_before_rule.addEventListener('click', function(e) {
+			fold_expend_all(json_data, true, '条');
+		});
+		div_fold_all.style.cssText = `
+			display : inline-block;
+			margin-left : 4em;
+			color : blue;
+			font-size : 24px;
+			cursor : pointer;
+		`;
+		div_fold_all.textContent = '折叠全部';
+		div_expend_all.style.cssText = `
+			display : inline-block;
+			margin-left : 20px;
+			color : blue;
+			font-size : 24px;
+			cursor : pointer;
+		`;
+		div_expend_all.textContent = '展开全部';
+		div_expend_before_rule.style.cssText = `
+			display : inline-block;
+			margin-left : 20px;
+			color : blue;
+			font-size : 24px;
+			cursor : pointer;
+		`;
+		div_expend_before_rule.textContent = '展开到条之前';
+
+		root_div.appendChild(div_fold_all);
+		root_div.appendChild(div_expend_all);
+		root_div.appendChild(div_expend_before_rule);
+
+		const write_content = (data, parend_dom, level_deep) => {
+			const div = document.createElement('div');
+			const div_content = document.createElement('div');
+			const div_children = document.createElement('div');
+			div.appendChild(div_content);
+			div.appendChild(div_children);
+			let a = data.text_content;
+			if (!Array.isArray(a)) {
+				a = [
+					{
+						p : 0,
+						t : a
+					},
+				];
+			}
+			a.forEach((e) => {
+				const line = document.createElement('div');
+				const pv = e.p ? e.p + 1 : 0;
+				line.style.marginLeft = pv + 'em';
+				line.textContent = e.t;
+				div_content.appendChild(line);
+			});
+			data.dom = {
+				current_dom : div,
+				content_dom : div_content,
+				children_dom : div_children,
+			};
+			parend_dom.appendChild(div);
+
+			if (data.children && data.children.length) {
+				div_content.addEventListener('click', function(e) {
+					if (data.children_hidden) {
+						div_content.style.removeProperty('color');
+						div_children.style.removeProperty('display');
+						data.children_hidden = false;
+					} else {
+						data.children_hidden = true;
+						div_children.style.display = 'none';
+						div_content.style.color = 'blue';
+					}
+				});
+				div_content.classList.add('class___mouse_style');
+				fold_data_array.push(data);
+
+				data.children.forEach((e) => {
+					write_content(e, div_children, level_deep + 1);
+				});
+			}
+		}
+		write_content(json_data, root_div, 1);
+
+	}());
+}
